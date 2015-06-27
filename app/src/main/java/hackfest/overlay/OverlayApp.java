a4daef7f42e995124c7c1187af24e3acf194cded
Angie Sun <sunangel@sunangel-macbookpro.roam.corp.google.com>
2015-06-27 12:52:54 -0700

    add parse

---
+++ app/src/main/java/hackfest/overlay/OverlayApp.java
@@ -0,0 +1,19 @@
+package hackfest.overlay;
+
+import android.app.Application;
+
+/**
+ * Created by sunangel on 6/27/15.
+ */
+public class OverlayApp extends Application {
+    @Override public void onCreate() {
+        super.onCreate();
+
+// Enable Local Datastore.
+        //Parse.enableLocalDatastore(this);
+
+        //Parse.initialize(this, "APPLICATION ID", "CLIENT KEY");
+    }
+
+}
+
