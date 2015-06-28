7dcfed687cca22ee11da94a44e6e8e04639dda52
dariaevdo <evdokimova@college.harvard.edu>
2015-06-27 17:06:06 -0700

    saving the overlayed image (not tested yet)

--- app/src/main/java/hackfest/overlay/ChooseOverlayActivity.java
+++ app/src/main/java/hackfest/overlay/ChooseOverlayActivity.java
@@ -1,10 +1,20 @@
 package hackfest.overlay;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -15,14 +25,20 @@ public class ChooseOverlayActivity extends ActionBarActivity {
     @InjectView(R.id.overlay) ImageView mOverlayPng;
     @InjectView(R.id.selected_photo) ImageView mSelectedPhoto;
 
+    private final String TAG = ChooseOverlayActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_overlay);
         ButterKnife.inject(this);
-        /*
-        Bitmap selectedPhotoBitmap = ((BitmapDrawable) mSelectedPhoto.getDrawable()).getBitmap();
-        Bitmap overlayBitmap = ((BitmapDrawable) mSelectedPhoto.getDrawable()).getBitmap();
+        saveOverlayedImage();
+    }
+
+    private void saveOverlayedImage() {
+        // processes two images, merges them and saves the result
+        Bitmap selectedPhotoBitmap = imageViewToBitmap(mSelectedPhoto);
+        Bitmap overlayBitmap = imageViewToBitmap(mOverlayPng);
 
         Bitmap bmOverlay = Bitmap.createBitmap(selectedPhotoBitmap.getWidth(),
                 selectedPhotoBitmap.getHeight(), selectedPhotoBitmap.getConfig());
@@ -31,10 +47,31 @@ public class ChooseOverlayActivity extends ActionBarActivity {
         canvas.drawBitmap(selectedPhotoBitmap, new Matrix(), null);
         canvas.drawBitmap(overlayBitmap, new Matrix(), null);
         canvas.save();
-        // TODO figure out how to save it
-        // this image view doesn't do anything right now
-        ImageView testImage = new ImageView(this);
-        testImage.setImageBitmap(bmOverlay); */
+
+        // saving the bitmap
+        try {
+            String fileName = Environment.getExternalStorageDirectory().toString();
+            File overlayedImageFile = new File(fileName, "overlayedImage.png");
+            overlayedImageFile.createNewFile();
+            Log.i(TAG, "file created " + overlayedImageFile.toString());
+            FileOutputStream out = new FileOutputStream(overlayedImageFile);
+            bmOverlay.compress(Bitmap.CompressFormat.PNG, 90, out);
+        } catch (IOException e) {
+            Log.e(TAG, "ERROR");
+            e.printStackTrace();
+        }
+    }
+
+    private Bitmap imageViewToBitmap(ImageView imageView) {
+        imageView.setDrawingCacheEnabled(true);
+        // Without it the view will have a dimension of 0,0 and the bitmap will be null
+        imageView.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        imageView.layout(0, 0, imageView.getMeasuredWidth(),
+                imageView.getMeasuredHeight());
+
+        imageView.buildDrawingCache();
+        return imageView.getDrawingCache();
     }
 
 
