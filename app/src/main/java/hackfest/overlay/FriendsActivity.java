3cd894bfd5c62b0e681cc7a3768977a79757795d
Angie Sun <sunangel@sunangel-macbookpro.roam.corp.google.com>
2015-06-28 06:16:04 -0700

    friends activity stuff

---
+++ app/src/main/java/hackfest/overlay/FriendsActivity.java
@@ -0,0 +1,35 @@
+package hackfest.overlay;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import hackfest.overlay.bottom_sheet_lib.MySimpleArrayAdapter;
+
+
+public class FriendsActivity extends ListActivity {
+    public ListView mListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+        //getSupportActionBar().hide();
+        mListView = (ListView) findViewById(R.id.listview);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        MySimpleArrayAdapter adapter = new MySimpleArrayAdapter(this, values);
+        setListAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/hackfest/overlay/bottom_sheet_lib/MySimpleArrayAdapter.java
@@ -0,0 +1,44 @@
+package hackfest.overlay.bottom_sheet_lib;
+
+/**
+ * Created by sunangel on 6/28/15.
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import hackfest.overlay.R;
+
+public class MySimpleArrayAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final String[] values;
+
+    public MySimpleArrayAdapter(Context context, String[] values) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.rowlayout, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        textView.setText(values[position]);
+        // change the icon for Windows and iPhone
+        String s = values[position];
+        if (s.startsWith("iPhone")) {
+            imageView.setImageResource(R.drawable.angie_head_circle);
+        } else {
+            imageView.setImageResource(R.drawable.com_facebook_button_check);
+        }
+
+        return rowView;
+    }
+}
